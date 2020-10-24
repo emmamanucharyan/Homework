ab8fcb596db05fcab1b3ace77556ba3ad647d050
emmamanucharyan <61308481+emmamanucharyan@users.noreply.github.com>
2020-10-24 17:11:17 +0400

    Update EmmaManucharyanGradedHW.py

--- EmmaManucharyanGradedHW.py
+++ EmmaManucharyanGradedHW.py
@@ -22,7 +22,7 @@ class Dequeue:
             temp.append(None)
             self.queue = temp
             j += 1
-        print(self.queue)
+        print("Resize:", self.queue, "\n")
         self.front = 0
         self.rear = initial_length-1
 
@@ -143,7 +143,7 @@ class DoubleLinkedList:
         while current is not None:
             print(str(current.data))
             current = current.next
-        print()    
+        print()
 
     def insert_first(self, obj):
         if self.size == 0:
@@ -299,6 +299,7 @@ class DoubleLinkedList:
             return
         if self.size == 1:
             return
+        temp = self.first
         current = self.first
         after_current = current.next
         current.next = None
@@ -309,6 +310,7 @@ class DoubleLinkedList:
             current = after_current
             after_current = after_current.previous
         self.first = current
+        self.last = temp
 
     def remove_numeric_nodes(self):
         if self.size == 0:
@@ -366,7 +368,7 @@ class DoubleLinkedList:
         else:
             return False
 
-    ############    No. 4 with test   ###############
+    ############    No. 4  with test  ###############
 
     def split_even_odd_into_new_lists(self):
         even_dllist = DoubleLinkedList()
@@ -409,17 +411,10 @@ class DoubleLinkedList:
 
 dllist = DoubleLinkedList()
 
-dllist.insert_first(3)
-dllist.insert_first(2)
-dllist.insert_first(1)
-dllist.insert_last(4)
-dllist.insert_last(5)
-dllist.insert_last(6)
-dllist.print_list()
-dllist.split_even_odd_into_new_lists()
-print(dllist.first)
+
 
 ############      No. 2      ###############
+
 
 class DeQueue(DoubleLinkedList):
 
@@ -428,6 +423,7 @@ class DeQueue(DoubleLinkedList):
         self.first = None
         self.last = None
         self.size = 0
+
 
     def display(self):
         self.print_list()
@@ -448,13 +444,13 @@ class DeQueue(DoubleLinkedList):
     def enqueue_back(self, obj):
         self.insert_last(obj)
 
-    def get_front(self):
+    def get_back(self):
         if self.is_empty():
             return None
         print(self.last.data)
         return self.last.data
 
-    def get_back(self):
+    def get_front(self):
         if self.is_empty():
             return None
         print(self.first.data)
@@ -474,16 +470,93 @@ class DeQueue(DoubleLinkedList):
 
 
 dque = DeQueue()
-dque.insert_first(3)
-dque.insert_first(2)
-dque.insert_first(1)
-dque.insert_last(4)
-dque.insert_last(5)
-dque.insert_last(6)
-
-
 
 
 #############      No. 5         ##############
 
+def main():
+    def test_array_dequeue():
+        global que
+        print("TESTING 1, ARRAY_DEQUEUE \n")
+        que.insert_front(1)
+        print("First is:", que.get_first())
+        if que.get_first() == 1:
+            print("True")
+        que.display_queue()
+        que.insert_end(2)
+        print("Last is:", que.get_last())
+        if que.get_last() == 2:
+            print("True")
+        que.display_queue()
+        que.insert_end(3)
+        que.display_queue()
+        que.remove_front()
+        que.display_queue()
+        print("First is:", que.get_first())
+        if que.get_first() == 2:
+            print("True")
 
+
+    def test_double_linked_list():
+        global dllist
+        print("TESTING 3, Double Linked List \n")
+        dllist.insert_first("w")
+        dllist.insert_last(5)
+        dllist.insert_first("a")
+        dllist.insert_first(2)
+        dllist.insert_last("b")
+        dllist.insert_first(0)
+        print("List is: 0<=>2<=>a<=>w<=>5<=>b", "\t Let's Check")
+        dllist.print_list()
+        print(dllist.test_first(0))
+        print(dllist.test_last("b"))
+        dllist.remove_by_value("w")
+        dllist.remove_by_value("b")
+        dllist.insert_before(5, 4)
+        dllist.print_list()
+        print("Reversed List:")
+        dllist.reverse_list()
+        dllist.print_list()
+        print(dllist.test_first(5))
+        print(dllist.test_last(0))
+
+    def test_split_list():
+        print("TESTING 4, Double Llist Split \n")
+        dlist = DoubleLinkedList()
+        dlist.insert_first(3)
+        dlist.insert_first(2)
+        dlist.insert_first(1)
+        dlist.insert_last(4)
+        dlist.insert_last(5)
+        dlist.insert_last(6)
+        dlist.print_list()
+        dlist.split_even_odd_into_new_lists()
+        print(dllist.first)
+
+
+    def test_dllist_dequeue():
+        global dque
+        print("TESTING 2, Double Llist_DEQUEUE \n")
+        dque.insert_first(3)
+        dque.insert_first(2)
+        dque.insert_first(1)
+        dque.insert_last(4)
+        dque.insert_last(5)
+        dque.insert_last(6)
+        dque.print_list()
+
+        dque.enqueue_front(8)
+
+        dque.dequeue_back()
+        dque.print_list()
+        print("Front is:")
+        dque.get_front()
+        print("End is:")
+        dque.get_back()
+
+    test_array_dequeue()
+    test_double_linked_list()
+    test_split_list()
+    test_dllist_dequeue()
+
+main()
